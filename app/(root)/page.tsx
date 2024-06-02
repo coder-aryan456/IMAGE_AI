38ca5225e94803c38134b6bbf52d4c75700d414c
coder-aryan456 <aryanlegendry0@gmail.com>
2024-06-02 20:21:01 +0530

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+const page = () => {
+  return (
+    <div>pagebvv b
+      {/* <UserButton afterSignOutUrl='/'/> */}
+    </div>
+  )
+}
+export default page
